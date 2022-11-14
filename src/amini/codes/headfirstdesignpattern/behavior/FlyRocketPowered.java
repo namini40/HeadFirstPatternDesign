44acc01789b97146ce8f536a9b21b6aebd081393
Nasser Amini <namini40@gmail.com>
2022-11-14 12:40:17 +0330

    add another `Duck` and another `FlyBehavior`

---
+++ src/amini/codes/headfirstdesignpattern/behavior/FlyRocketPowered.java
@@ -0,0 +1,8 @@
+package amini.codes.headfirstdesignpattern.behavior;
+
+public class FlyRocketPowered implements FlyBehavior{
+    @Override
+    public void fly() {
+        System.out.println("I`m flying with a rocket!");
+    }
+}
