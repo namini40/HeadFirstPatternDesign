9971632b3e73fad0b8eaddf62909217f8163722b
Nasser Amini <namini40@gmail.com>
2022-11-15 20:42:16 +0330

    add  as a decorator to understand  build-in java objects

--- src/amini/codes/headfirstpatterndesign/Run.java
+++ src/amini/codes/headfirstpatterndesign/Run.java
@@ -1,5 +1,10 @@
 package amini.codes.headfirstpatterndesign;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Run {
     public static void main(String[] args) {
         Beverage beverage = new Espresso();
@@ -11,7 +16,6 @@ public class Run {
         beverage2 = new Mocha(beverage2);
         System.out.println(beverage2.getDescription() + ", " + beverage2.cost());
 
-
         Beverage beverage3 = new HouseBlend();
 
         beverage3 = new Mocha(beverage3);
@@ -19,6 +23,23 @@ public class Run {
         beverage3 = new Whip(beverage3);
 
         System.out.println(beverage3.getDescription() + ", " + beverage3.cost());
+
+        int c;
+        try {
+            String dir = 
+                    System.getProperty("user.dir");
+
+            InputStream in = new LowerCaseInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(dir+"/test.txt")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
