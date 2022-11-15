59baabdaf87be0315297571287c0b9f13bd7559d
Nasser Amini <namini40@gmail.com>
2022-11-15 12:46:58 +0330

    test the design

--- src/amini/codes/headfirstdesignpatterns/Run.java
+++ src/amini/codes/headfirstdesignpatterns/Run.java
@@ -2,6 +2,12 @@ package amini.codes.headfirstdesignpatterns;
 
 public class Run {
     public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionsDisplay display = new CurrentConditionsDisplay(weatherData);
+        
+        weatherData.setMeasurements(23.12f, 13.56f, 67.89f);
+        weatherData.setMeasurements(26.11f, 63.55f, 92.52f);
+        weatherData.setMeasurements(33.17f, 15.59f, 78.43f);
 
     }
 }
